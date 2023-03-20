1467d9d28bc2667371a24c0a0efeffffcd6cac2e
dahee-heo <hdahee.9@gmail.com>
2023-03-20 20:19:35 +0900

    stitches 세팅

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Items from './pages/Items';
 import Groceries from './pages/Groceries';
-import Header from './components/Header';
+import { Header } from './components/Header';
 import { Suspense } from 'react';
 import { useRecoilState } from 'recoil';
 import { users } from './states/userState'
+import { Add } from './pages/Add';
+import { Container } from './styles/components/Container';
+import NavBar from './components/NavigationBar';
 
 function App() {
   const [loginUser, setLoginUser] = useRecoilState(users)
@@ -16,13 +18,15 @@ function App() {
   return (
     <>
       <Suspense fallback={<div>Loading...</div>}>
-        <Header uid={uid} />
-        <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/home' element={<Home />}></Route>
-          <Route path='/items' element={<Items uid={uid} />}></Route>
-          <Route path='/groceries' element={<Groceries uid={uid} />}></Route>
-        </Routes>
+        <Container>
+          <Header uid={uid} />
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/home' element={<Home />}></Route>
+            <Route path='/items' element={<Items uid={uid} />}></Route>
+            <Route path='/groceries' element={<Groceries uid={uid} />}></Route>
+          </Routes>
+        </Container>
       </Suspense>
     </>
   );
---
+++ src/pages/Add.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '../styles/components/Button'
+
+
+export const Add = () => {
+  return (
+    <>
+      <Button bg="primary" sizes="large">button</Button>
+    </>
+
+  )
+}
