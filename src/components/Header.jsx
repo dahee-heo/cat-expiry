09e9c27111e76b70e1e447c9ee64dd3f245a76b4
dahee-heo <hdahee.9@gmail.com>
2023-04-10 20:14:13 +0900

    다국어 모드 토글 버튼

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ import { ReactComponent as Logo } from '../assets/Logo.svg';
 import { Sidebar } from './MypageModal';
 import { useNavigate } from 'react-router-dom';
 import { ArrowBackIos } from '@material-ui/icons';
+import i18n from '../config/lang/i18n';
 
 const Header = () => {
   const [loginUser, setLoginUser] = useRecoilState(users)
@@ -14,18 +15,41 @@ const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false)
   const handleOpen = () => setMenuOpen(true)
   const navigation = useNavigate()
+  const [openLng, setOpenLng] = useState(false)
+  let classActive = openLng ? "active" : "";
+  const locale = localStorage.getItem("locale") ?? "ko";
+  const [browserLang, setBrowserLang] = useState("KOR")
+
+  const handleChangeLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+    setOpenLng(false)
+    localStorage.setItem("locale", lang)
+    lang === "ko" ? setBrowserLang("KOR") : setBrowserLang("ENG")
+  }
+
+  useEffect(()=>{
+    locale === "ko" || "ko-KR" 
+      ? i18n.changeLanguage("ko") 
+      : i18n.changeLanguage("en");
+  }, [])
 
   return (
     <>
       <div className='header'>
         <div className='header__wrap'>
           <h1><Logo/></h1>
-          <div className='global'>
-            <div className=''>
-              <img src="" alt="" /> KOR <span></span></div>
+          <div 
+            className={`global ${classActive}`} 
+          >
+            <div className='' onClick={() => setOpenLng(!openLng)}>
+              {browserLang} <span className='arrow'></span></div>
             <ul className='slide'>
-              <li><img src={'../assets/kor.png'} alt="" /> KOR</li>
-              <li><img src="" alt="" /> ENG</li>
+              <li onClick={()=>handleChangeLanguage("ko")}>
+                KOR
+              </li>
+              <li onClick={()=>handleChangeLanguage("en")}>
+                ENG
+              </li>
             </ul>
           </div>
         </div>
